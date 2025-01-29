1179a5051db9e2e7f2bf49ddd3d3b85dbad7ded6
Vortex-71 <vikashkr1717@gmail.com>
2025-01-30 04:24:29 +0530

    enhancement: Add heatmap, motion detection & Discord alerts

--- train_yolo.py
+++ train_yolo.py
@@ -1,2 +1,8 @@
-import torch
-torch.cuda.empty_cache()
+from ultralytics import YOLO
+
+def train():
+    model = YOLO("yolov8m.pt")  # Load YOLO model
+    model.train(data="/content/Crowd-Management-System/dataset/data.yaml", epochs=120, imgsz=640, batch=8, device="cuda", half=True)
+
+if __name__ == "__main__":
+    train()
